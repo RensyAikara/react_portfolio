944bd908e937bb4eb797f3aac818bbbf92cababe
Rensy <rensy.aikara@gmail.com>
2020-05-27 23:01:00 -0500

    Updates

---
+++ precache-manifest.f7de2ce8cddf355363e17bb30fa679bd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49fb0bdaa34280197b50fe6ef3562e95",
+    "url": "/react_portfolio/index.html"
+  },
+  {
+    "revision": "f5b308f36e36c1b63e58",
+    "url": "/react_portfolio/static/css/main.38de81b0.chunk.css"
+  },
+  {
+    "revision": "923e0fb2f51e14c13f3f",
+    "url": "/react_portfolio/static/js/2.5b560ce3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react_portfolio/static/js/2.5b560ce3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5b308f36e36c1b63e58",
+    "url": "/react_portfolio/static/js/main.4ac092b2.chunk.js"
+  },
+  {
+    "revision": "6b43b2f91fa6da009663",
+    "url": "/react_portfolio/static/js/runtime-main.9b257cb4.js"
+  },
+  {
+    "revision": "a9ddebe5c8dbfbd190c74b37a10e8037",
+    "url": "/react_portfolio/static/media/bookshelf.a9ddebe5.png"
+  },
+  {
+    "revision": "706874d8b62886ca6f675f0f9141ae76",
+    "url": "/react_portfolio/static/media/weatherEats-04.706874d8.png"
+  }
+]);
